f023b566048d783e09d2a8241391f109aabd0d11
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-06-28 17:19:03 -0300

    feat: create API with Mongo conection

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from 'cors';
+import { MongoClient, ObjectId } from "mongodb";
+import joi from 'joi';
+import dotenv from 'dotenv';
+import dayjs from "dayjs";
+import chalk from 'chalk';
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(process.env.MONGO_URL)
+let db;
+client.connect().then(() => {
+    db = client.db(process.env.MONGO_DATABASE_NAME)
+})
+
+
+const PORT = process.env.PORT
+console.log(process.env.MONGO_URL)
+app.listen(PORT, () => {
+    console.log(chalk.cyanBright(`Server running on ${process.env.MONGO_URL}`))
+})
